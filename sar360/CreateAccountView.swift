f5f044598b4ca51d0dea4f76199d2e6cad042f8a
Annika Whitwam <awhit169@uottawa.ca>
2024-06-22 13:06:14 -0400

    create project, simple login page

---
+++ sar360/CreateAccountView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateAccountView.swift
+//  sar360
+//
+//  Created by Annika Whitwam on 2024-06-22.
+//
+
+import SwiftUI
+
+struct CreateAccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateAccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateAccountView()
+    }
+}
---
+++ sar360/LoginView.swift
@@ -0,0 +1,63 @@
+//
+//  LoginView.swift
+//  sar360
+//
+//  Created by Annika Whitwam on 2024-06-22.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Login")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                
+                HStack {
+                    Image(systemName: "person")
+                        .foregroundColor(.gray)
+                    TextField("Username", text: $username)
+                }
+                .padding(.vertical, 10)
+                .overlay(Rectangle().frame(height: 1).padding(.top, 35), alignment: .bottom)
+                .padding(.horizontal, 50)
+                            
+                HStack {
+                    Image(systemName: "lock")
+                        .foregroundColor(.gray)
+                    SecureField("Password", text: $password)
+                }
+                .padding(.vertical, 10)
+                .overlay(Rectangle().frame(height: 1).padding(.top, 35), alignment: .bottom)
+                .padding(.horizontal, 50)
+                
+                NavigationLink(destination: CreateAccountView()){
+                    Text("Login")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding(EdgeInsets(top: 15, leading: 70, bottom: 15, trailing: 70))
+                        .background(Color.green)
+                        .cornerRadius(30)
+                        .shadow(radius: 5)
+                        .padding()
+                }
+                NavigationLink(destination: CreateAccountView()){
+                    Text("Don't have an account? Register now")
+                        .foregroundColor(Color.green)
+                        .multilineTextAlignment(.center)
+                }
+            }
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
