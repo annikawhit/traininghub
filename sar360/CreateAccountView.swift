ca16ec19d0968ca3245b0578415689984866e888
Annika Whitwam <awhit169@uottawa.ca>
2024-06-22 22:08:27 -0400

    login page ui and being create account page

--- sar360/CreateAccountView.swift
+++ sar360/CreateAccountView.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct CreateAccountView: View {
+    @State private var fname: String = ""
+    @State private var lname: String = ""
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var cpassword: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextField("First Name", text: $fname)
+                .padding()
+                .background(Color("CustomBrown"))
+                .foregroundColor(Color.white)
+                .cornerRadius(5)
+                .padding()
+            
+            TextField("Last Name", text: $lname)
+                .padding()
+                .background(Color("CustomBrown"))
+                .foregroundColor(Color.white)
+                .cornerRadius(5)
+                .padding()
+            
+            TextField("Email", text: $email)
+                .padding()
+                .background(Color("CustomBrown"))
+                .foregroundColor(Color.white)
+                .cornerRadius(5)
+                .padding()
+            
+            TextField("Password", text: $password)
+                .padding()
+                .background(Color("CustomBrown"))
+                .foregroundColor(Color.white)
+                .cornerRadius(5)
+                .padding()
+            
+            TextField("Confirm Password", text: $cpassword)
+                .padding()
+                .background(Color("CustomBrown"))
+                .foregroundColor(Color.white)
+                .cornerRadius(5)
+                .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .edgesIgnoringSafeArea(.all)
+        .background(Color("CustomBackground"))
     }
 }
 
--- sar360/LoginView.swift
+++ sar360/LoginView.swift
@@ -9,49 +9,73 @@ import SwiftUI
 
 
 struct LoginView: View {
-    @State private var username: String = ""
+    @State private var email: String = ""
     @State private var password: String = ""
     
     var body: some View {
+        
         NavigationView {
-            VStack {
-                Text("Login")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
+            ZStack {
+                GeometryReader { geometry in
+                    VStack(spacing: 0) {
+                        Color("CustomGreen")
+                            .frame(height: geometry.size.height / 3)
+                        Color("CustomBackground")
+                    }
+                }
+                .edgesIgnoringSafeArea(.all)
                 
-                HStack {
-                    Image(systemName: "person")
-                        .foregroundColor(.gray)
-                    TextField("Username", text: $username)
+                VStack {
+                    Text("logo here")
+                        .font(.system(size: 50))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("CustomBackground"))
+                        .padding(.bottom, 180)
+                    
+                    Text("Sign in")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding(.bottom, 10)
+                    
+                    HStack {
+                        Image(systemName: "person")
+                            .foregroundColor(.gray)
+                        TextField("Email", text: $email)
+                    }
+                    .padding(.vertical, 10)
+                    .overlay(Rectangle().frame(height: 1)
+                        .padding(.top, 35), alignment: .bottom)
+                    .padding(.horizontal, 50)
+                    
+                    HStack {
+                        Image(systemName: "lock")
+                            .foregroundColor(.gray)
+                        SecureField("Password", text: $password)
+                    }
+                    .padding(.vertical, 10)
+                    .overlay(Rectangle().frame(height: 1).padding(.top, 35), alignment: .bottom)
+                    .padding(.horizontal, 50)
+                    
+                    NavigationLink(destination: CreateAccountView()){
+                        Text("Login")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding(EdgeInsets(top: 15, leading: 70, bottom: 15, trailing: 70))
+                            .background(Color("CustomBrown"))
+                            .cornerRadius(15)
+                            .shadow(radius: 5)
+                            .padding(.top, 35)
+                            .padding(.bottom, 10)
+                    }
+                    NavigationLink(destination: CreateAccountView()){
+                        Text("Don't have an account? Register now")
+                            .foregroundColor(Color("CustomBrown"))
+                            .multilineTextAlignment(.center)
+                            .padding(.bottom, 100)
+                    }
                 }
-                .padding(.vertical, 10)
-                .overlay(Rectangle().frame(height: 1).padding(.top, 35), alignment: .bottom)
-                .padding(.horizontal, 50)
-                            
-                HStack {
-                    Image(systemName: "lock")
-                        .foregroundColor(.gray)
-                    SecureField("Password", text: $password)
-                }
-                .padding(.vertical, 10)
-                .overlay(Rectangle().frame(height: 1).padding(.top, 35), alignment: .bottom)
-                .padding(.horizontal, 50)
-                
-                NavigationLink(destination: CreateAccountView()){
-                    Text("Login")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .padding(EdgeInsets(top: 15, leading: 70, bottom: 15, trailing: 70))
-                        .background(Color.green)
-                        .cornerRadius(30)
-                        .shadow(radius: 5)
-                        .padding()
-                }
-                NavigationLink(destination: CreateAccountView()){
-                    Text("Don't have an account? Register now")
-                        .foregroundColor(Color.green)
-                        .multilineTextAlignment(.center)
-                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .edgesIgnoringSafeArea(.all)
             }
         }
     }
